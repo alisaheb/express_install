ef75919d2df060fccf0cd88c16ec045d3fbb54ff
Ali Saheb <alisaheb44@gmail.com>
2018-02-24 15:50:42 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+app.use(express.static(__dirname+'/public'));
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('copyrightDate',()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('uppercase',(text)=>{
+  return text.toUpperCase();
+});
+
+//middle ware
+app.use((req,res,next)=>{
+  console.log(`${req.method}`);
+  var log = `method: ${req.method} request: ${req.url}`
+  fs.appendFile('server.log',log+'\n');
+  next();
+});
+//get request
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle:'This is about page',
+    copyright:new Date().getFullYear()
+  });
+});
+app.get('/about',(req,res)=>{
+  res.send({
+    ali:'working',
+    saheb:[
+      'he is not a good prrson',
+      'only name is saheb'
+    ]
+  });
+});
+app.get('/abouts',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'This is about page',
+    copyright:new Date().getFullYear()
+  });
+});
+app.listen(3000,()=>{
+  console.log('server is up on the port 3000');
+});
